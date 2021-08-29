a2bba7946591236a124910fec3225221f59f29d1
sahumo <7523460@gmail.com>
2021-08-29 04:32:15 +0300

    метод превращает строку все заглавные буквы

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const str = "test";
-const arr = [1, 2, 3];
+// const arr = [1, 2, 3];
 
 
-console.log(str[2]); // shous   s   because   s is number  2  in  word  "test" >>>>>>t=0,e=1,s=2,....
+// console.log(str[2]); // shous   s   because   s is number  2  in  word  "test" >>>>>>t=0,e=1,s=2,....
 
 
+console.log(str.toUpperCase()); // метод превращает строку все заглавные буквы
+console.log(str);
