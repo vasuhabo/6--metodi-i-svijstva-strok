5192fd765aa0b9a2bb6de80fc1b7f782c7856bd4
sahumo <7523460@gmail.com>
2021-08-29 04:16:00 +0300

    str.lenght

--- script.js
+++ script.js
@@ -1,61 +1,9 @@
 'use strict';
 
-let num = 20;// global variable
-
-function showFirstMessage(text)
-{console.log(text); console.log(num); }//num = 10; local variable 
+const str = "test";
+const arr = [1, 2, 3];
 
 
-
-
-showFirstMessage("Hello world!");// takes argument from here and puts in line 3 instead text,after puts it in line 4 instead text
-console.log(num);
-
-// result---->  Hello world! 20 20
-
-function calc(a, b)
-
-{return (a + b);}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-
-
-function ret()
-
-{ let num = 50; return num;}
-
-const anotherNum = ret();
-
-console.log(anotherNum);
-
-// result---->  50
-
-const logger = function ()
-
-{console.log("Hello") ;};
-
-logger();
-
-
-// funkzii   strelochnie
-
-const kalc = (a, b) => { return a + b };
-
-const kkalc = (a, b) => a + b ;
-
-const kddalc = a => a + b ;
-
-
-const kafgdflc = (a, b) => {console.log; return a+b; };
-
-
-
-
-
-
-
+console.log(str.length);
 
 
